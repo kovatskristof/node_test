81988edda5ad642f0e6b20dd8aa92a8ea1c8240b
EMEA\Z0004707 <kristof.kovats@zf.com>
2020-08-10 14:24:49 +0200

    webhook pretest

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! I wanna see some changes here: Here you go!');
+  res.send('Hello World! I wanna see some changes here.');
 });
 
 const PORT = process.env.PORT || 4000;
